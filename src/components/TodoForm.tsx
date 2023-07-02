56d386a11649a611a84f40f1441652ef220da2c3
Akbermet198 <122314250+Akbermet198@users.noreply.github.com>
2023-07-02 22:09:29 +0600

    ts-todo

---
+++ src/components/TodoForm.tsx
@@ -0,0 +1,21 @@
+
+interface formProps  {
+ value: string,
+  updateText: (str: string)=>void,
+   handleAction: ()=>void
+}
+
+const NewTodoForm: React.FC<formProps> = ({ value,updateText,handleAction }) => {
+ return (
+   <label>
+     <input
+       placeholder='new todo'
+       value={value}
+       onChange={(e) => updateText(e.target.value)}
+     />
+     <button onClick={handleAction}>Add todo</button>
+   </label>
+ );
+};
+
+export default NewTodoForm;
